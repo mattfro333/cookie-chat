0b700e5a019ebc31777818aa246c4bad005ef48f
mattfro333 <mattfro333@gmail.com>
2017-01-18 16:58:20 -0700

    Https vs. Http

--- js/messageService.js
+++ js/messageService.js
@@ -4,7 +4,16 @@ angular.module('chatroom').service('messageService', function($http){
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'http://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
+this.getMessages = function(){
+        return $http({
+          method: 'Get',
+          url: 'http://practiceapi.devmounta.in/api/chats'
+        })
+        .then(function(response){
+  return response.data;
+})
 
+}
 
 
 
@@ -12,7 +21,20 @@ angular.module('chatroom').service('messageService', function($http){
   //The url for the request needs to be 'http://practiceapi.devmounta.in/api/chats'
   //Because we're making a POST request, we need a way to tell the server the data we want to give it, in your $http call (along with url and method) have a data property which has a value that is equal to another object with a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+this.postMessage = function(message){
+          return $http({
+            method: 'POST',
+            url: 'https://practiceapi.devmounta.in/api/chats',
+            data: {
+              message:message
+            }
 
+          })
+          .then(function(response){
+    return response.data;
+  })
+
+}
 
 
 
